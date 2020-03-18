c9b75697c15d4c3a13d832d3614025d51b164b11
Jaksa Vuckovic <jaksa76@gmail.com>
2020-03-18 17:12:26 +0100

    simplified scrolling logic in textfield

--- textfield.go
+++ textfield.go
@@ -37,10 +37,6 @@ func (t *TextField) handleInput(input *userInput) {
 			if t.cursorPosition > 0 {
 				t.content = t.content[:t.cursorPosition-1] + t.content[t.cursorPosition:]
 				t.cursorPosition--
-
-				if t.contentOffset > 0 {
-					t.contentOffset--
-				}
 			}
 		} else if repeatingKeyPressed(ebiten.KeyDelete) && t.content != "" {
 			if t.cursorPosition < len(t.content) {
@@ -49,35 +45,26 @@ func (t *TextField) handleInput(input *userInput) {
 		} else if repeatingKeyPressed(ebiten.KeyLeft) {
 			if t.cursorPosition > 0 {
 				t.cursorPosition--
-				if t.cursorPosition < t.contentOffset {
-					t.contentOffset--
-				}
 			}
 		} else if repeatingKeyPressed(ebiten.KeyRight) {
 			if t.cursorPosition < len(t.content) {
 				t.cursorPosition++
-
-				if t.cursorPosition > t.contentOffset+t.bounds.Dx()/6-1 {
-					t.contentOffset++
-				}
 			}
 		} else if repeatingKeyPressed(ebiten.KeyHome) {
 			t.cursorPosition = 0
-			t.contentOffset = 0
 		} else if repeatingKeyPressed(ebiten.KeyEnd) {
 			t.cursorPosition = len(t.content)
-			t.contentOffset = len(t.content) - (t.bounds.Dx())/6 + 1
-			if t.contentOffset < 0 {
-				t.contentOffset = 0
-			}
 		} else {
 			added := ebiten.InputChars()
 			t.content = t.content[:t.cursorPosition] + string(added) + t.content[t.cursorPosition:]
 			t.cursorPosition += len(added)
+		}
 
-			if t.cursorPosition > t.bounds.Dx()/6-1 {
-				t.contentOffset += len(added)
-			}
+		// scroll to cursor
+		if t.cursorPosition < t.contentOffset {
+			t.contentOffset = t.cursorPosition
+		} else if t.cursorPosition > t.contentOffset+t.widthInChars() {
+			t.contentOffset = t.cursorPosition - t.widthInChars()
 		}
 	}
 }
@@ -94,7 +81,6 @@ func (t *TextField) draw(img *ebiten.Image) {
 	ebitenutil.DebugPrintAt(img, truncToPixels(t.content[t.contentOffset:], b.Dx()), b.Min.X+3, b.Min.Y+b.Dy()/2-8)
 
 	if t.hasFocus {
-		// cursorX := float64(b.Min.X + 5 + len(truncToPixels(t.content, b.Dx()))*6)
 		cursorX := float64(b.Min.X + 5 + (t.cursorPosition-t.contentOffset)*6)
 		cursorY := float64(b.Min.Y + b.Dy()/2 - 6)
 		ebitenutil.DrawLine(img, cursorX, cursorY, cursorX, cursorY+12.0, color.White)
@@ -108,6 +94,10 @@ func (t *TextField) draw(img *ebiten.Image) {
 	ebitenutil.DrawLine(img, x1, y2-1, x2, y2-1, borderLightColor) // bottom
 	ebitenutil.DrawLine(img, x1+1, y1, x1+1, y2, boderDarkColor)   // left
 	ebitenutil.DrawLine(img, x2, y1, x2, y2, borderLightColor)     // right
+}
+
+func (t *TextField) widthInChars() int {
+	return t.bounds.Dx()/6 - 1
 }
 
 func truncToPixels(s string, pixels int) string {
